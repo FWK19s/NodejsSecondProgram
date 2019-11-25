29a22b1521cb314140f14f0ed754d44595ba8160
Taleb_84 <taleb_n1@hotmail.com>
2019-11-25 21:57:24 +0100

    create an async function
    create an async function

--- app.js
+++ app.js
@@ -4,7 +4,14 @@
 //     res.end('Hello Node');
 // }).listen(8080);
 
+// const fs = require('fs');
+// const output = fs.readFileSync('output.txt');
+// console.log(output.toString());
+// console.log('End of the program');
+
 const fs = require('fs');
-const output = fs.readFileSync('output.txt');
-console.log(output.toString());
-console.log('End of the program');
+fs.readFile('output.txt', function (err, me){
+    if (err) return console.error(err);
+    console.log(me.toString());
+});
+console.log('end of the program');
