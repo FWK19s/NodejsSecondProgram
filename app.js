d4047f5d052b5806ab8d1d6f5f1ff4b4aa2c2261
Taleb_84 <taleb_n1@hotmail.com>
2019-11-25 22:32:01 +0100

    delet output.txt
    delete a file using node

--- app.js
+++ app.js
@@ -16,14 +16,24 @@
 // });
 // console.log('end of the program');
 
-const fs = require('fs');
-console.log('We are going to get file info!');
-fs.stat('output.txt', function (err, stats){
-if (err) {return console.error(err)
-}
-console.log(stats);
-console.log('this is the file info');
-// check the file type
-console.log('isFile ?' + stats.isFile());
-console.log('isDirectory' + stats.isDirectory());
+// const fs = require('fs');
+// console.log('We are going to get file info!');
+// fs.stat('output.txt', function (err, stats){
+// if (err) {return console.error(err)
+// }
+// console.log(stats);
+// console.log('this is the file info');
+// // check the file type
+// console.log('isFile ?' + stats.isFile());
+// console.log('isDirectory' + stats.isDirectory());
+// });
+
+const fs = require("fs");
+
+console.log("Going to delete an existing file");
+fs.unlink('output.txt', function(err) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log("File deleted successfully!");
 });
