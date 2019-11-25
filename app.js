11713f62c9b0c8122f31b70b1268f7d6535d9c2c
Taleb_84 <taleb_n1@hotmail.com>
2019-11-25 22:27:01 +0100

    get the file information
    create a function to get the file information

--- app.js
+++ app.js
@@ -9,9 +9,21 @@
 // console.log(output.toString());
 // console.log('End of the program');
 
+// const fs = require('fs');
+// fs.readFile('output.txt', function (err, me){
+//     if (err) return console.error(err);
+//     console.log(me.toString());
+// });
+// console.log('end of the program');
+
 const fs = require('fs');
-fs.readFile('output.txt', function (err, me){
-    if (err) return console.error(err);
-    console.log(me.toString());
+console.log('We are going to get file info!');
+fs.stat('output.txt', function (err, stats){
+if (err) {return console.error(err)
+}
+console.log(stats);
+console.log('this is the file info');
+// check the file type
+console.log('isFile ?' + stats.isFile());
+console.log('isDirectory' + stats.isDirectory());
 });
-console.log('end of the program');
