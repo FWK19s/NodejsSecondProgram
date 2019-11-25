a0a58971710d93e6d4e4ed0d91998dd806e41691
Taleb_84 <taleb_n1@hotmail.com>
2019-11-25 21:54:10 +0100

    create example syn program
    create a sync function

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-const http = require('http');
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-type': 'text/html'});
-    res.end('Hello Node');
-}).listen(8080)
+// const http = require('http');
+// http.createServer(function (req, res){
+//     res.writeHead(200, {'Content-type': 'text/html'});
+//     res.end('Hello Node');
+// }).listen(8080);
+
+const fs = require('fs');
+const output = fs.readFileSync('output.txt');
+console.log(output.toString());
+console.log('End of the program');
